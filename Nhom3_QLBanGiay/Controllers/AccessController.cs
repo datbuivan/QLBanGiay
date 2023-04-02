994f2fe6cc4b79095b33ae890a855dba3325c76f
Quangduc2002 <phamquangduc110@gmail.com>
2023-04-02 17:10:33 +0700

    login admin and user

--- Nhom3_QLBanGiay/Controllers/AccessController.cs
+++ Nhom3_QLBanGiay/Controllers/AccessController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Nhom3_QLBanGiay.Models;
 
 namespace Nhom3_QLBanGiay.Controllers
@@ -9,13 +11,13 @@ namespace Nhom3_QLBanGiay.Controllers
 
         public IActionResult Login()
         {
-            if (HttpContext.Session.GetString("UserName") == null)
+            if ( HttpContext.Session.GetString("UserName") == null)
             {
-                return View();
+                    return View();
             }
             else
             {
-                return RedirectToAction("Index", "Home");
+                  return RedirectToAction("Index", "Home");
             }
         }
 
@@ -27,20 +29,37 @@ namespace Nhom3_QLBanGiay.Controllers
             if (HttpContext.Session.GetString("UseHome") == null)
             {
                 var u = db.Users.Where(x => x.UserName == user.UserName && x.PassWord == user.PassWord).FirstOrDefault();
+                var role = db.Users.Where(x => x.UserName == user.UserName);
+
                 if (u != null)
                 {
-                    HttpContext.Session.SetString("UserName", u.UserName.ToString());
-                    return RedirectToAction("Index", "Home");
+                        HttpContext.Session.SetString("UserName", u.UserName.ToString());
+                        HttpContext.Session.SetString("Role", u.Role.ToString());
+                        if(HttpContext.Session.GetString("Role") == "0")
+                        {
+                            return View();
+                        }else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
                 }
             }
             return View(user);
+
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Login", "access");
+            }
         }
 
+
         public IActionResult Logout()
-        {
-            HttpContext.Session.Clear();
-            HttpContext.Session.Remove("UserName");
-            return RedirectToAction("Login", "access");
+            {
+                HttpContext.Session.Clear();
+                HttpContext.Session.Remove("UserName");
+                return RedirectToAction("Login", "access");
+            }
         }
-    }
 }
